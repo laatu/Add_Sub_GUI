801669e94dac5c21b81466a323118004b1ac810d
laatuji <laatuji@laatu>
2016-01-14 01:27:50 +0530

    First commit ... here  ... setting up the gui form again  in github .... deleted the gui-f from github....

---
+++ src/gui/Gui.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import gui.Cal;
+
+/**
+ *
+ * @author laatuji
+ */
+public class Gui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Cal c=new Cal();
+        c.setVisible(true);
+    }
+    
+}
